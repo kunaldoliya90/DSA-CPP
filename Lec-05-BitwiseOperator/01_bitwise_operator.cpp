fc54cf6dda795c4d72a2889b846d8be458754f75
kunaldoliya90 <kunal.doliya90@gmail.com>
2023-05-11 01:42:36 +0530

    Bitwise Operator

---
+++ Lec-05-BitwiseOperator/01_bitwise_operator.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+/*
+
+AND -> &
+OR -> |
+NOT -> ~
+XOR -> ^
+
+*/
+
+int main()
+{
+    // int a = 4;
+    // int b = 6;
+
+    // cout << " a&b is :" << (a & b) << endl;
+    // cout << " a|b is : " << (a | b) << endl;
+    // cout << " ~a is : " << (~a) << endl;
+    // cout << " a^b is : " << (a ^ b) << endl;
+
+    /*
+
+    Left Shift Operator
+    Example - 5<<1 { 5 ko ek baar left shift kar do}
+    Before left shift--->00000000---00101
+    After  left shift--->00000000---01010
+
+    Example - 3<<2 { 3 ko 2 baar left shift kar do}
+    Before left shift--->00000000---00011
+    After  left shift--->00000000---01100
+
+    ( Left shift 1 bare kare toh multiply by 2 agar do bar kar rahe toh multiply by 2*2)
+
+
+    Right Shift Operator
+    Example - 5>>1 { 5 ko ek baar right shift kar do}
+    Before Right shift--->00000000---00101
+    After  Right shift--->00000000---00001
+
+    Example - 3>>2 { 3 ko 2 baar right shift kar do}
+    Before Right shift--->00000000---00011
+    After  Right shift--->00000000---01100 -> 3/2-> 1/2 -> 0
+
+    ( Right shift 1 bare kare toh divide by 2 agar do bar kar rahe toh divide by 2*2)
+
+    */
+
+    /*Example of right shift and left shift operator*/
+
+    // cout << (17 >> 1) << endl;
+    // cout << (17 << 1) << endl;
+    // cout << (17 >> 2) << endl;
+    // cout << (17 << 2) << endl;
+    // cout << (19 >> 1) << endl;
+    // cout << (19 << 1) << endl;
+    // cout << (19 >> 2) << endl;
+    // cout << (19 << 2) << endl;
+
+    
+
+
+}
